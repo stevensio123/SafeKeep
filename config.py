071c22f4f32de46b09aed8a0bad3f2acc5aa4329
fantoosh <avalon74@protonmail.com>
2022-12-03 19:52:34 +0100

    updated config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ DEBUG = True
 
 # Define the application directory
 import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
 
 
 class Config:
