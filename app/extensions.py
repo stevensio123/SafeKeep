70cafd7a9269e42775d1efccb5723c1bd25fc34f
fantoosh <avalon74@protonmail.com>
2022-12-03 17:00:21 +0100

    refactored code: Split code in __init__.py to functions

---
+++ app/extensions.py
@@ -0,0 +1,16 @@
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+moment = Moment()
+mail = Mail()
+
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.login_message = "Please login or signup to add a new trip"
+
--- app/models.py
+++ app/models.py
@@ -4,14 +4,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from flask_login import UserMixin
 
-from . import login_manager
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
 # Define a base model for other database tables to inherit
 class Base(db.Model, UserMixin):
 
