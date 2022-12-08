07fbfa6d87c7da16fe8a1b55537d9d13ef6111b2
Steven <stevenscw3253@gmail.com>
2022-12-08 23:17:52 +0800

    newest pgenerator

--- app/pgenerator/views.py
+++ app/pgenerator/views.py
@@ -2,29 +2,29 @@ from flask_login import login_required
 from flask import Blueprint, flash ,redirect, render_template, request, url_for
 from . import pgenerator
 import random
-import string
 
 
 @pgenerator.route('/pgenerator', methods=['GET', 'POST'])
 def pgenerate():
     
     if request.method == "POST":
-        length = request.form.get('length')
+        length = int(request.form.get("length"))
         
-        if length >= 8 and length <= 16:
-          
-            return render_template('pgenerator.html',password=pgen(length))
+        if length >= 8 and length <= 40:
+            password = pgen(length)
+            return render_template('pgenerator.html',password=password)
         
     
-    return render_template('pgenerator.html',password=pgen(13))  #set 13 as the default length of generated password
+    return render_template('pgenerator.html',password=pgen(int(13)))  #set 13 as the default length of generated password
+
 
 
 def pgen(length):
-    #get a string of all characters
-    char_seq = str(string.printable)
+    #A string of all usable characters
+    char_seq = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()-?@[]^_`~"
     password =''
 
-    for len in range(length):
+    for len in range(int(length)):
         random_char = random.choice(char_seq)
         password += random_char
             
