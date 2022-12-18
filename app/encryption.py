4f3977b771f41513d81b5a9beb812186bf3364fb
fantoosh <avalon74@protonmail.com>
2022-12-18 15:46:15 +0100

    modified:   app/encryption.py

--- app/encryption.py
+++ app/encryption.py
@@ -13,63 +13,6 @@ load_dotenv()
 
 SALT = os.getenv('SECRET_KEY').encode()
 
-"""
-# Generate a random salt
-salt = Fernet.generate_key()
-
-# Define the password to be encrypted
-password = 'mysecretpassword'.encode('utf-8')
-
-# Define the master password and the number of iterations to use
-# for the key derivation function
-master_password = b'pbkdf2:sha256:260000$I2juRBxKTf4XaW3J$b7bad70d601b1fcb7e992651d22d7db116c2aea7ab946c16df84db7bb6412fd1'
-iterations = 100000
-
-# Use PBKDF2HMAC to derive a key from the master password and salt
-kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    iterations=iterations
-)
-key = kdf.derive(master_password)
-key = base64.urlsafe_b64encode(key)
-
-# Use the key to encrypt the password
-fernet = Fernet(key)
-encrypted_password = fernet.encrypt(password)
-
-# Store the encrypted password, salt, and iteration count in a database
-# or other storage location
-
-# To decrypt the password, retrieve the encrypted password, salt, and
-# iteration count from the database and use them to derive the key
-kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    iterations=iterations
-)
-key = kdf.derive(master_password)
-
-# Encode the key using URL-safe base64 encoding
-key = base64.urlsafe_b64encode(key)
-
-# Use the key to decrypt the password
-fernet = Fernet(key)
-decrypted_password = fernet.decrypt(encrypted_password)
-
-# The decrypted password should match the original password
-assert decrypted_password == password
-# This code uses the PBKDF2HMAC key derivation function from the cryptography library to derive a
-# key from the user's master password and a random salt. The password is then encrypted using this key and the Fernet symmetric encryption algorithm. The encrypted password, salt, and iteration count are stored in a database or other storage location. When the password is needed, the salt and iteration count are retrieved and used to derive the key, and the password is decrypted using the key.
-
-# This is just an example of how AES-256 bit encryption could be used to save login credentials.
-# There are many other ways to implement this, and the specific details may vary depending on your application. This code should not be considered a complete and secure solution.
-# print(decrypted_password, ':', password)
-
-
-"""
 
 def encrypt_credential(password: str, master_password: str) -> str:
 
